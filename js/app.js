8f89c5103964a2cfa90d61ec39fff7b9cd95f7fe
Firas Abah <fabah@isu.net.sa>
2021-06-06 14:55:05 +0300

    Updated files..

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
 // app.js
+var counter = 0;
+resetCounter();
+function resetCounter() {
+    document.querySelector("#counter").innerHTML = 0;
+}
+function counterUp() {
+    document.querySelector("#counter").innerHTML = ++counter;
+}
